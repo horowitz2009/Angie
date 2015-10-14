6d85480a9ea141c17cf81b2f21b3e560c2d5f947
Rosen <ros.hristov@gmail.com>
2015-10-14 18:45:17 +0300

    Product page updated
    Product code hidden
    
    Share buttons hidden until made to work
    
    Delivery data changed

--- index.php
+++ index.php
@@ -84,6 +84,17 @@
 </head>
 
 <body ng-controller="MainCtrl">
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.5";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+
   <!-- Modal Reset Password -->
   <div class="modal signUpContent fade" id="ModalResetPassword" tabindex="-1" role="dialog">
     <div class="modal-dialog ">
